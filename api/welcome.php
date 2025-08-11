931265a7b026509852443aa0aa193a98ad0d9dc3
Teknary <felopate1er16@gmail.com>
2025-08-11 21:00:15 +0300

    issue

--- api/welcome.php
+++ api/welcome.php
@@ -1,10 +1,8 @@
 <?php
 session_start();
 if (!isset($_SESSION['username'])) {
-    header("Location: login.php");
+    header("Location: https://skilldr.vercel.app/api/login.php");
     exit();
 }
 ?>
-
 <h1>Welcome, <?php echo $_SESSION['username']; ?>!</h1>
-<a href="logout.php">Logout</a>
