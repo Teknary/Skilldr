c252340232f3581197052e84206b7096bf5da08c
Teknary <felopate1er16@gmail.com>
2025-09-02 12:36:02 +0300

    somefucks

--- api/welcome.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['username'])) {
-    header("Location: https://skilldr.vercel.app/api/login.php");
-    exit();
-}
-?>
-<h1>Welcome, <?php echo $_SESSION['username']; ?>!</h1>
